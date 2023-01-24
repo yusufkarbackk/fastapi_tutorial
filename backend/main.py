d0fde905327b0d78c7544d7d07677ad3913c8e30
Yusuf karback <yusufkarback64@gmail.com>
2023-01-24 17:31:53 +0700

    add logo for the navbar

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,23 @@
 from fastapi import FastAPI
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
+from fastapi.staticfiles import StaticFiles
 
 
 def include_router(app):
-	app.include_router(general_pages_router)
+    app.include_router(general_pages_router)
+
+
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 def start_application():
-	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
-	include_router(app)
-	return app 
+    app = FastAPI(title=settings.PROJECT_NAME,
+                  version=settings.PROJECT_VERSION)
+    include_router(app)
+    configure_static(app)
+    return app
 
 
 app = start_application()
